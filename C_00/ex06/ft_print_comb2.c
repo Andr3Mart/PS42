50d18a7e74d3fb46a13d6864ba79d51294afc205
Andre Martins <anmartin@c2r1s7.42lisboa.com>
2021-01-11 13:35:26 +0000

    ex 06

---
+++ C_00/ex06/ft_print_comb2.c
@@ -0,0 +1,63 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_comb2.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: anmartin <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/01/11 13:23:06 by anmartin          #+#    #+#             */
+/*   Updated: 2021/01/11 13:28:07 by anmartin         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	printdigits(int n)
+{
+	if (n < 10)
+	{
+		ft_putchar('0');
+		ft_putchar(n + '0');
+	}
+	else
+	{
+		ft_putchar(n / 10 + '0');
+		ft_putchar(n % 10 + '0');
+	}
+}
+
+void	printnum(int c1, int c2)
+{
+	printdigits(c1);
+	ft_putchar(' ');
+	printdigits(c2);
+	if (c1 != 98 || c2 != 99)
+	{
+		ft_putchar(',');
+		ft_putchar(' ');
+	}
+}
+
+void	ft_print_comb2(void)
+{
+	int i;
+	int j;
+
+	i = 0;
+	while (i <= 99)
+	{
+		j = i;
+		while (j <= 99)
+		{
+			if (i != j)
+				printnum(i, j);
+			++j;
+		}
+		++i;
+	}
+}
