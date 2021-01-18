d22c05fa6627200dd68c76fee41b5e6542b93c1c
Andr3Mart <31989200+Andr3Mart@users.noreply.github.com>
2021-01-18 09:09:21 +0000

    Update ex02 ft_str_is_alpha.c

--- C02/ex02 ft_str_is_alpha.c
+++ C02/ex02 ft_str_is_alpha.c
@@ -1,12 +1,16 @@
 int		ft_str_is_alpha(char *str)
 {
 	int		i;
+	char		c;
 
 	i = 0;
 	while (str[i] != '\0')
 	{
-		if (!((str[i] >= 'A' && str[i] <= 'Z') || (str[i] >= 'a' && str[i] <= 'z')))
+		c = str[i];
+		if (!((c >= 'A' && c <= 'Z') || c >= 'a' && c <= 'z')))
+		{
 			return (0);
+		}
 		i++;
 	}
 	return (1);
